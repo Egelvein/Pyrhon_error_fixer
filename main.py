b6a6a53c293447f82ec98dafecc1025ea5b3ecb7
Vyacheslav <sisla00@ya.ru>
2024-12-30 23:36:55 +0800

    new model 1

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from api_key_file import GOOGLE_API_KEY
 
 app = FastAPI()
 
-kg_path = "../Datasets/preprocessed_data/rag_data.json"
+kg_path = "Datasets/preprocessed_data/rag_data.json"
 
 chat_history_path = "chat_history.json"
 prompt_for_model_path = "prompt_for_model.json"
@@ -40,7 +40,6 @@ with open(kg_path, "r") as file:
 
 embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
 index = faiss.read_index("rag_base/retriever_index.faiss")
-# generator = pipeline("text2text-generation", model="google/flan-t5-base")
 
 genai.configure(api_key=GOOGLE_API_KEY)
 model = genai.GenerativeModel('gemini-1.5-flash')
@@ -52,23 +51,11 @@ class QueryRequest(BaseModel):
 def retrieve_examples(query, top_k=5):
     query_embedding = embedding_model.encode([query], convert_to_numpy=True)
     query_embedding = query_embedding / np.linalg.norm(query_embedding)
-
     distances, indices = index.search(query_embedding, top_k)
-    # print(f"distances: {distances}")
-    # print(f"indices: {indices}")
-    # print(f"returned: {[data[idx] for idx in indices[0]]}")
+
     return [data[idx] for idx in indices[0]]
 
 def generate_response(query, retrieved_examples):
-
-    ### Original version
-    #context = "\n".join(
-        #[f"Example {i+1}:\nPrompt: {ex['prompt']}\nInput: {ex['input']}\nExplanation: {ex['explanation']}"
-         #for i, ex in enumerate(retrieved_examples)]
-    #)
-
-
-    ### Slava changed
     context = "\n".join(
         [f"Example {i+1}:\nInput: {ex['input']}\nExplanation: {ex['explanation']}\nCorrect code for this example: {ex['correct_code']}"
          for i, ex in enumerate(retrieved_examples)]
@@ -96,15 +83,8 @@ Below is the user's broken Python code, along with relevant examples of similar 
 2. Corrected Code: Provide the corrected version of the user's code based on the selected example."""
     )
 
-    #response = generator(combined_input,
-                         #max_length=2048,
-                         #do_sample=True,
-                         #top_p=0.95,
-                         #temperature=0.25)
-
     response = model.generate_content(combined_input)
 
-    # return response[0]['generated_text'], combined_input
     return response.text, combined_input
 
 def save_chat_history(user_input, bot_response):
@@ -113,14 +93,6 @@ def save_chat_history(user_input, bot_response):
         "user_input": user_input,
         "bot_response": bot_response
     }
-
-    #try:
-        #with open(chat_history_path, "r") as file:
-            #history = json.load(file)
-    #except FileNotFoundError:
-        #history = []
-
-    #history.append(entry)
 
     with open(chat_history_path, "a") as file:
         json.dump(entry, file, indent=4)
@@ -132,31 +104,14 @@ def save_full_prompt(full_prompt):
         "full_input": full_prompt
     }
 
-    #try:
-        #with open(prompt_for_model_path, "r") as file:
-            #history = json.load(file)
-    #except FileNotFoundError:
-        #history = []
-
-    #history.append(entry)
-
     with open(prompt_for_model_path, "a") as file:
         json.dump(entry, file, indent=4)
 
-
-#@app.post("/analyze")
-#def analyze_code(request: QueryRequest):
-    #retrieved_examples = retrieve_examples(request.query)
-    #response = generate_response(request.query, retrieved_examples)
-    #save_chat_history(request.query, response)
-    #return {"response": response}
-
 def gradio_interface(user_query):
     retrieved_examples = retrieve_examples(user_query)
-    # print(f"retrieved example: {retrieved_examples}")
     response, full_prompt = generate_response(user_query, retrieved_examples)
     save_chat_history(user_query, response)
-    save_full_prompt(full_prompt, )
+    save_full_prompt(full_prompt)
     return response
 
 # Gradio UI
@@ -167,6 +122,7 @@ interface = gr.Interface(
     title="Code Analysis Chatbot",
     description="Paste your broken code here"
 )
+
 
 # API endpoint
 @app.get("/")
